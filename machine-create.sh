e7bff1b110eebb19fae84349752094bbad30ab25
Alexander Rysenko <rysenko@gmail.com>
2015-11-03 10:39:54 +0200

    Added machine-remove script

--- machine-create.sh
+++ machine-create.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
 SWARM_TOKEN=`docker run swarm create`
 docker-machine create -d digitalocean --digitalocean-access-token=$DIGITALOCEAN_ACCESS_TOKEN --swarm --swarm-master --swarm-discovery token://$SWARM_TOKEN host-1
 docker-machine create -d digitalocean --digitalocean-access-token=$DIGITALOCEAN_ACCESS_TOKEN --swarm --swarm-discovery token://$SWARM_TOKEN host-2
+eval $(docker-machine env --swarm host-1)
+docker-compose build
+docker-compose up -d
---
+++ machine-remove.sh
@@ -0,0 +1,2 @@
+docker-machine rm host-2
+docker-machine rm host-1
